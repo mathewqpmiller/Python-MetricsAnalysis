ea4e23c2891d3ff5cba8e99f721350bd44f6735f
mathewqpmiller <mathewpqmiller@gmail.com>
2021-04-18 12:46:41 -0700

    PyPoll finished code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,7 +5,7 @@ import os
 import csv
 
 # Path Directory
-PyPollcsv = os.path.join("Resources","election_data.csv")
+py_poll_csv = os.path.join("Resources","election_data.csv")
 
 # Create Lists
 candidate_list = []
@@ -16,60 +16,59 @@ vote_percent = []
 # Initialize Variable
 count = 0
 
-# Read CSV
-with open(PyPollcsv, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvreader)
+# Define Function
+def get_results(data):
     
     # For Loop
-    for row in csvreader:
+    for row in data:
         # Count Iteration
-        count = count + 1
-        # Append Candidates
-        candidate_list.append(row[2])
-    # For Loop
-    for x in set(candidate_list):
-        # Append Names
-        unique_candidate.append(x)
+        count += 1
+        # If Statement: Append Names
+        if row[2] not in unique_candidate:
+            unique_candidate.append(row[2])
         # Count Votes
-        y = candidate_list.count(x)
-        # Append Count
-        vote_count.append(y)
-        # Calculate Vote Percentage
-        z = (y/count)*100
-        # Append Vote Percentage
-        vote_percent.append(z)
-        
-    winning_vote_count = max(vote_count)
-    winner = unique_candidate[vote_count.index(winning_vote_count)]
+        vote_count.append(row[2])
+
+    # For Loop
+    for candidate in unique_candidate:
+        # Candidate Vote Counts
+        candidate_list.append(vote_count.count(candidate))
+        # Candidate Vote Percentage
+        vote_percent.append(round(vote_count.count(candidate)/count*100,3))
+
+    # Poll Winner
+    winner = unique_candidate[candidate_list.index(max(candidate_list))]
     
-# Note to TA: I have tried several ways to get the max of the votecount list and retrieve the name as Winner. But unsucessful. 
-# Hence I am leaving that part out of this code. But Khan is the winner, I know!!!!
-# Jake suggested: votecount = votecount["percentage"].sort_values()
-# Print to terminal
-# Output perhaps needs to be rounded to 3 decimal points. Leaving that formatting out for now) 
- 
-print("-------------------------")
-print("Election Results")   
-print("-------------------------")
-print("Total Votes :" + str(count))    
-print("-------------------------")
-for i in range(len(unique_candidate)):
-            print(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote_count[i])+ ")")
-print("-------------------------")
-print("The winner is: " + winner)
-print("-------------------------")
+    # print results, use a loop for the number of uniqueCandidates
+    print('PyPoll Election Results')
+    print('--------------------------------')
+    print(f'Total Votes: {count}')
+    print('--------------------------------')
+    for i in range(len(unique_candidate)):
+        print(f'{unique_candidate[i]}: {vote_percent[i]}% {candidate_list[i]}')
+    print('--------------------------------')
+    print(f'Winner: {winner}')
+    print('--------------------------------')
 
-# Print to a text file: election_results.txt
-# Output perhaps needs to be rounded to 3 decimal points. Leaving that formatting out for now) 
+    # Export Path
+    poll_output = os.path.join("election_results.txt")
 
-with open('election_results.txt', 'w') as text:
-    text.write("Election Results\n")
-    text.write("---------------------------------------\n")
-    text.write("Total Vote: " + str(count) + "\n")
-    text.write("---------------------------------------\n")
-    for i in range(len(set(unique_candidate))):
-        text.write(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote_count[i]) + ")\n")
-    text.write("---------------------------------------\n")
-    text.write("The winner is: " + winner + "\n")
-    text.write("---------------------------------------\n")
+    # Export Print
+    with open(poll_output, "w") as txtfile:
+        txtfile.write('PyPoll Election Results')
+        txtfile.write('\n------------------------------------')
+        txtfile.write(f'\nTotal Votes: {count}')
+        txtfile.write('\n------------------------------------')
+        for i in range (len(unique_candidate)):
+            txtfile.write(f'\n{unique_candidate[i]}: {vote_percent[i]}% {candidate_list[i]}')
+        txtfile.write('\n------------------------------------')
+        txtfile.write(f'\nWinner: {winner}')
+        txtfile.write('\n------------------------------------')
+
+# Read CSV
+with open(py_poll_csv, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    # Account for Header
+    csv_header = next(csvfile)
+    # Call Function
+    get_results(csv_reader)
